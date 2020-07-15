9625fd5477b1c20fb024063500fb83f0c32ffe1a
Cosmo Menezes de Souza <65937470+cosmo-menezes@users.noreply.github.com>
2020-07-15 10:38:02 -0300

    Aula11.tiposDeVariaveis

---
+++ aula11/CuriosidadeInt.java
@@ -0,0 +1,20 @@
+package com.loiane.cursojava.aula11;
+
+public class CuriosidadeInt {
+
+	public static void main(String[] args) {
+		
+		int var1 = 2147483647;
+		
+		int var2 = 1;
+		
+		System.out.println(var1 + var2);
+		
+		// Dica: quando somamos um numero ao valor maximo do caractere //
+		// esse numero volta para o menor valor negativo //
+		// Pegadinha de entrevista de emprego //
+		
+
+	}
+
+}
---
+++ aula11/VariaveisChar.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		char o = 'o';
+		char i = 'i';
+		char interrogacao = 00111111;
+		
+		System.out.println(o + i);
+		System.out.println(""+o+i+interrogacao);
+		
+
+	}
+
+}
